f21ef0d659fbaa7aa9686434eba5d45b5e9f75b3
213181712 <62132666+213181712@users.noreply.github.com>
2025-11-27 13:50:34 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,13 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+    file = open(file_path)
+    rows = file.readlines()
+    file.close()
+
+    codons_dict = {}
+    for row in rows:
+        row = row.strip()
+        cells = row.split("\t")
+        codons_dict.update({cells[0]:cells[2]})
+    return codons_dict
 
 
